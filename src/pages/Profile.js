635445a87b2ceab2f74ed387615f32fcb8498c89
Zhaksybay Sayat <sayat.zhaksybay@narxoz.kz>
2024-11-18 01:52:42 +0500

    Изменения дизайна профиля

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -24,7 +24,7 @@ const UserDashboard = () => {
                 setProfile(response.data);
                 setLoadingProfile(false);
             } catch (err) {
-                setProfileError('Failed to load profile data');
+                setProfileError('Не удалось загрузить данные профиля');
                 setLoadingProfile(false);
             }
         };
@@ -69,18 +69,18 @@ const UserDashboard = () => {
     };
 
     const handleChangePassword = async () => {
-        setPasswordMessage(''); // Clear any previous messages
-    
+        setPasswordMessage(''); // Очистить любые предыдущие сообщения
+
         if (!oldPassword || !newPassword || !confirmPassword) {
             setPasswordMessage('Пожалуйста, заполните все поля.');
             return;
         }
-    
+
         if (newPassword !== confirmPassword) {
             setPasswordMessage('Новый пароль и подтверждение пароля не совпадают.');
             return;
         }
-    
+
         try {
             const response = await api.post('/change_password/', {
                 old_password: oldPassword,
@@ -89,90 +89,92 @@ const UserDashboard = () => {
             });
             setPasswordMessage(response.data.message || 'Пароль успешно изменен.');
         } catch (err) {
-            console.error("Full error response:", err.response); // Log full error details
+            console.error("Full error response:", err.response); // Логирование полной ошибки
             setPasswordMessage(
                 err.response?.data?.error || 'Ошибка при изменении пароля. Пожалуйста, попробуйте снова.'
             );
         }
     };
-    
 
     return (
         <div className="dashboard-container">
-            <div className="profile-section">
-                <h2>Профиль</h2>
-                {loadingProfile ? (
-                    <div>Загрузка...</div>
-                ) : profileError ? (
-                    <div className="error">{profileError}</div>
-                ) : (
-                    profile && (
-                        <div className="profile-info">
-                            <div className="profile-field">
-                                <span className="label">Имя:</span>
-                                <span className="value">{profile.first_name}</span>
+            <div className="profile-wrapper">
+                <p className="profile-subtitle">Студент университета NARXOZ</p>
+                <div className="profile-content">
+                    <div className="card profile-section">
+                        <h2>Профиль</h2>
+                        {loadingProfile ? (
+                            <p>Загрузка...</p>
+                        ) : profileError ? (
+                            <p className="error">{profileError}</p>
+                        ) : (
+                            <div className="profile-info">
+                                <div className="profile-field">
+                                    <span className="label">Имя:</span>
+                                    <span className="value">{profile.first_name}</span>
+                                </div>
+                                <div className="profile-field">
+                                    <span className="label">Фамилия:</span>
+                                    <span className="value">{profile.last_name}</span>
+                                </div>
+                                <div className="profile-field">
+                                    <span className="label">Email:</span>
+                                    <span className="value">{profile.email}</span>
+                                </div>
+                                <div className="profile-field">
+                                    <span className="label">ID студента:</span>
+                                    <span className="value">{profile.s}</span>
+                                </div>
+                                <div className="profile-field">
+                                    <span className="label">Телефон:</span>
+                                    <span className="value">{profile.phone}</span>
+                                </div>
                             </div>
-                            <div className="profile-field">
-                                <span className="label">Фамилия:</span>
-                                <span className="value">{profile.last_name}</span>
-                            </div>
-                            <div className="profile-field">
-                                <span className="label">Email:</span>
-                                <span className="value">{profile.email}</span>
-                            </div>
-                            <div className="profile-field">
-                                <span className="label">ID студента:</span>
-                                <span className="value">{profile.s}</span>
-                            </div>
-                            <div className="profile-field">
-                                <span className="label">Телефон:</span>
-                                <span className="value">{profile.phone}</span>
-                            </div>
-                        </div>
-                    )
-                )}
-            </div>
-
-            <div className="status-section">
-                <h2>Статус Заявки</h2>
-                {statusError && <p className="error">{statusError}</p>}
-                {status && <p className="status">{status}</p>}
-
-                {status === 'Заявка одобрена, внесите оплату и прикрепите скрин.' && (
-                    <div className="upload-section">
-                        <h3>Загрузите скриншот оплаты</h3>
-                        <input type="file" onChange={handleFileChange} />
-                        <button onClick={handleUpload} className="upload-button">Загрузить</button>
-                        {uploadMessage && <p className="upload-message">{uploadMessage}</p>}
+                        )}
                     </div>
-                )}
-            </div>
 
-            <div className="change-password-section">
-                <h2>Изменить Пароль</h2>
-                <input
-                    type="password"
-                    placeholder="Старый пароль"
-                    value={oldPassword}
-                    onChange={(e) => setOldPassword(e.target.value)}
-                    className="password-input"
-                />
-                <input
-                    type="password"
-                    placeholder="Новый пароль"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    className="password-input"
-                />
-                <input
-                    type="password"
-                    placeholder="Подтвердите новый пароль"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    className="password-input"
-                />
-                <button onClick={handleChangePassword} className="change-password-button">Изменить Пароль</button>
-                {passwordMessage && <p className="password-message">{passwordMessage}</p>}
+                    <div className="card status-section">
+                        <h2>Статус Заявки</h2>
+                        {statusError && <p className="error">{statusError}</p>}
+                        {status && <p className="status">{status}</p>}
+
+                        {status === 'Заявка одобрена, внесите оплату и прикрепите скрин.' && (
+                            <div className="upload-section">
+                                <h3>Загрузите скриншот оплаты</h3>
+                                <input type="file" onChange={handleFileChange} />
+                                <button onClick={handleUpload} className="upload-button">Загрузить</button>
+                                {uploadMessage && <p className="upload-message">{uploadMessage}</p>}
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="card change-password-section">
+                        <h2>Изменить Пароль</h2>
+                        <input
+                            type="password"
+                            placeholder="Старый пароль"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                            className="password-input"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Новый пароль"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            className="password-input"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Подтвердите новый пароль"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            className="password-input"
+                        />
+                        <button onClick={handleChangePassword} className="change-password-button">Изменить Пароль</button>
+                        {passwordMessage && <p className="password-message">{passwordMessage}</p>}
+                    </div>
+                </div>
             </div>
         </div>
     );
