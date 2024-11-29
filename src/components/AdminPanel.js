a3984da6c35832e2eaf3aa00c1de24dc25c046a0
Assem Kassenova <assem.kassenova@narxoz.kz>
2024-11-29 23:30:54 +0500

    admin panel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/AdminPanel.css';
+import api from '../api';
 
 const AdminPanel = () => {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
-  const [approvedStudents, setApprovedStudents] = useState([]); 
-  const [allocatedStudents, setAllocatedStudents] = useState([]); 
+  const [allApplications, setAllApplications] = useState([]);
+  const [dormitories, setDormitories] = useState([]);
+  const [editingApplicationId, setEditingApplicationId] = useState(null); 
+
+  const fetchDormitories = async () => {
+    try {
+      const response = await api.get('/dormlist'); 
+      setDormitories(response.data);
+    } catch (error) {
+      console.error('Ошибка при загрузке списка общежитий:', error);
+      setMessage({ type: 'error', text: 'Ошибка при загрузке списка общежитий' });
+    }
+  };
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -17,20 +29,8 @@ const AdminPanel = () => {
     formData.append('file', file);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/v1/upload-excel/', {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('access')}`,
-        },
-        body: formData,
-      });
-
-      const data = await response.json();
-      if (response.ok) {
-        setMessage({ type: 'success', text: 'Данные успешно загружены и обновлены' });
-      } else {
-        setMessage({ type: 'error', text: data.error || 'Ошибка загрузки файла' });
-      }
+      const response = await api.post('/upload-excel/', formData);
+      setMessage({ type: 'success', text: 'Данные успешно загружены и обновлены' });
     } catch (error) {
       console.error('Ошибка при загрузке файла:', error);
       setMessage({ type: 'error', text: 'Ошибка при подключении к серверу' });
@@ -39,37 +39,84 @@ const AdminPanel = () => {
 
   const handleDistributeStudents = async (apiUrl) => {
     try {
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('access')}`,
-        },
-      });
-      const data = await response.json();
-
-      if (response.ok) {
-        setMessage({ type: 'success', text: data.detail || 'Студенты успешно распределены' });
-
-        if (apiUrl.includes('distribute-students/')) {
-          setApprovedStudents(data.approved_students || []); 
-        } else if (apiUrl.includes('distribute-students2/')) {
-          setAllocatedStudents(data.allocated_students || []); 
-        }
-      } else {
-        setMessage({ type: 'error', text: data.detail || 'Ошибка распределения студентов' });
-        if (apiUrl.includes('distribute-students2/')) {
-          setAllocatedStudents([]); 
-        } else {
-          setApprovedStudents([]);
-        }
-      }
+      const response = await api.post(apiUrl);
+      setMessage({ type: 'success', text: response.data.detail || 'Студенты успешно распределены' });
     } catch (error) {
       console.error('Ошибка при распределении студентов:', error);
       setMessage({ type: 'error', text: 'Ошибка при подключении к серверу' });
-      setApprovedStudents([]);
-      setAllocatedStudents([]);
     }
   };
+
+  const handleApproveApplication = async (applicationId) => {
+    try {
+      const response = await api.put(`/applications/${applicationId}/approve/`, {
+        status: 'approved',
+      });
+      setMessage({ type: 'success', text: 'Заявка успешно одобрена' });
+
+      setAllApplications((prevApplications) =>
+        prevApplications.map((app) =>
+          app.id === applicationId ? { ...app, status: 'approved' } : app
+        )
+      );
+    } catch (error) {
+      console.error('Ошибка при одобрении заявки:', error);
+      setMessage({ type: 'error', text: 'Ошибка при подключении к серверу' });
+    }
+  };
+
+  const handleChangeDormitory = async (applicationId, dormitoryName) => {
+    if (!dormitoryName) return;
+  
+    try {
+      const response = await api.put(`/applications/${applicationId}/change-dormitory/`, {
+        dorm_name: dormitoryName, 
+      });
+  
+      setMessage({ type: 'success', text: response.data.message });
+  
+      setAllApplications((prevApplications) =>
+        prevApplications.map((app) =>
+          app.id === applicationId ? { ...app, dormitory_choice: dormitoryName } : app
+        )
+      );
+  
+      setEditingApplicationId(null); 
+    } catch (error) {
+      console.error('Ошибка при изменении общежития:', error);
+      setMessage({ type: 'error', text: 'Ошибка при изменении общежития' });
+    }
+  };
+  
+  
+  const handleDeleteApplication = async (applicationId) => {
+    try {
+      await api.delete(`/applications/${applicationId}/delete/`);
+      setMessage({ type: 'success', text: 'Заявка успешно удалена' });
+
+      setAllApplications((prevApplications) =>
+        prevApplications.filter((app) => app.id !== applicationId)
+      );
+    } catch (error) {
+      console.error('Ошибка при удалении заявки:', error);
+      setMessage({ type: 'error', text: 'Ошибка при подключении к серверу' });
+    }
+  };
+
+  const fetchAllApplications = async () => {
+    try {
+      const response = await api.get('/applications');
+      setAllApplications(response.data.results || response.data);
+    } catch (error) {
+      console.error('Ошибка при загрузке заявок:', error);
+      setMessage({ type: 'error', text: 'Ошибка при подключении к серверу' });
+    }
+  };
+
+  useEffect(() => {
+    fetchAllApplications();
+    fetchDormitories();
+  }, []);
 
   return (
     <div className="admin-panel">
@@ -83,45 +130,66 @@ const AdminPanel = () => {
 
       <div className="section">
         <h2>Распределение студентов</h2>
-        <button onClick={() => handleDistributeStudents('http://127.0.0.1:8000/api/v1/distribute-students/')}>
+        <button onClick={() => handleDistributeStudents('/distribute-students/')}>
           Распределить студентов по заявкам
         </button>
-        <button onClick={() => handleDistributeStudents('http://127.0.0.1:8000/api/v1/distribute-students2/')}>
+        <button onClick={() => handleDistributeStudents('/distribute-students2/')}>
           Распределить студентов по комнатам
         </button>
       </div>
 
-      {message && (
-        <div className={message.type}>
-          {message.text}
-        </div>
-      )}
+      {message && <div className={message.type}>{message.text}</div>}
 
-      {approvedStudents.length > 0 && (
-        <div className="section">
-          <h2>Одобренные студенты</h2>
-          <ol>
-            {approvedStudents.map((student, index) => (
-              <li key={index}>
-                S: {student.student_s}, Имя: {student.first_name}, Фамилия: {student.last_name}, Курс: {student.course}
-              </li>
-            ))}
-          </ol>
-        </div>
-      )}
+      <div className="section">
+  <h2>Все заявки</h2>
+  {allApplications.length > 0 ? (
+    <ol>
+      {allApplications.map((app) => (
+        <li key={app.id}>
+          <p>Заявка ID: {app.id}</p>
+          <p>Студент: {app.student.first_name} {app.student.last_name}</p>
+          <p>Статус: {app.status}</p>
+          <p>Общежитие: {app.dormitory_choice || 'Не выбрано'}</p> {/* Название общежития */}
 
-      {allocatedStudents.length > 0 && (
-        <div className="section">
-          <h2>Распределённые студенты</h2>
-          <ol>
-            {allocatedStudents.map((student, index) => (
-              <li key={index}>
-                S: {student.student_s}, Имя: {student.first_name}, Фамилия: {student.last_name}, Комната: {student.room}, Общежитие: {student.dorm_id}
-              </li>
-            ))}
-          </ol>
-        </div>
-      )}
+
+          <button onClick={() => handleApproveApplication(app.id)}>
+            Одобрить заявку
+          </button>
+
+          {editingApplicationId === app.id ? (
+            <div>
+              <select
+                onChange={(e) => handleChangeDormitory(app.id, e.target.value)}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Выберите общежитие
+                </option>
+                {dormitories.map((dorm) => (
+                  <option key={dorm.id} value={dorm.name}>
+                    {dorm.name}
+                  </option>
+                ))}
+              </select>
+              <button onClick={() => setEditingApplicationId(null)}>Отменить</button>
+            </div>
+          ) : (
+            <button onClick={() => setEditingApplicationId(app.id)}>
+              Изменить общежитие
+            </button>
+          )}
+
+          <button onClick={() => handleDeleteApplication(app.id)}>
+            Удалить заявку
+          </button>
+        </li>
+      ))}
+    </ol>
+  ) : (
+    <p>Нет заявок для отображения.</p>
+  )}
+</div>
+
     </div>
   );
 };
